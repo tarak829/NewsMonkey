0a4377618b45a80bef82c3a81abb2160cb87d90b
tarak829 <abhishektarak45@gmail.com>
2022-03-02 18:58:43 +0530

    changed to functional based component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,69 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default class App extends Component {
+const App = () => {
+  let pageSize = 6;
+  let country = "in";
 
-  pageSize = 6;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={<News key='general' country={country} pageSize={pageSize} category='general' />}
+        />
+        <Route
+          exact
+          path='/business'
+          element={<News key='business' country={country} pageSize={pageSize} category='business' />}
+        />
+        <Route
+          exact
+          path='/entertainment'
+          element={
+            <News key='entertainment' country={country} pageSize={pageSize} category='entertainment' />
+          }
+        />
+        <Route
+          exact
+          path='/health'
+          element={<News key='health' country={country} pageSize={pageSize} category='health' />}
+        />
+        <Route
+          exact
+          path='/science'
+          element={<News key='science' country={country} pageSize={pageSize} category='science' />}
+        />
+        <Route
+          exact
+          path='/sports'
+          element={<News key='sports' country={country} pageSize={pageSize} category='sports' />}
+        />
+        <Route
+          exact
+          path='/technology'
+          element={<News key='technology' country={country} pageSize={pageSize} category='technology' />}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-  static propTypes = {
-    category: PropTypes.string.isRequired,
-    country: PropTypes.string.isRequired,
-    pageSize: PropTypes.number.isRequired,
-  }
+export default App;
 
-  static defaultProps = {
-    category: 'general',
-    country: 'in',
-    pageSize: 6,
-  }
+App.propTypes = {
+  category: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  pageSize: PropTypes.number.isRequired,
+};
 
-  render() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            exact
-            path='/'
-            element={<News key='general' country='in' pageSize = {this.pageSize} category='general' />}
-          />
-          <Route
-            exact
-            path='/business'
-            element={<News key='business' country='in' pageSize = {this.pageSize} category='business' />}
-          />
-          <Route
-            exact
-            path='/entertainment'
-            element={<News key='entertainment' country='in' pageSize = {this.pageSize} category='entertainment' />}
-          />
-          <Route
-            exact
-            path='/health'
-            element={<News key='health' country='in' pageSize = {this.pageSize} category='health' />}
-          />
-          <Route
-            exact
-            path='/science'
-            element={<News key='science' country='in' pageSize = {this.pageSize} category='science' />}
-          />
-          <Route
-            exact
-            path='/sports'
-            element={<News key='sports' country='in' pageSize = {this.pageSize} category='sports' />}
-          />
-          <Route
-            exact
-            path='/technology'
-            element={<News key='technology' country='in' pageSize = {this.pageSize} category='technology' />}
-          />
-        </Routes>
-      </Router>
-    );
-  }
-}
+App.defaultProps = {
+  category: "general",
+  country: "in",
+  pageSize: 6,
+};
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,77 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Badge, Card } from "react-bootstrap";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
+  let { title, description, urlToImage, publishedAt, url, author, source } = props;
 
-  static propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string,
-    urlToImage: PropTypes.string,
-    publishedAt: PropTypes.string,
-    url: PropTypes.string,
+  let calcTime = {
+    publishedTime: new Date(publishedAt).toLocaleString(),
+    currentTime: new Date().toLocaleString(),
+    timeDiff: Math.abs(new Date(publishedAt).getTime() - new Date().getTime()),
+    timeDiffInMinutes: Math.abs(Math.round((new Date(publishedAt).getTime() - new Date().getTime()) / 60000)),
+    timeDiffInHours: Math.abs(Math.round((new Date(publishedAt).getTime() - new Date().getTime()) / 3600000)),
+    timeDiffInDays: Math.abs(Math.round((new Date(publishedAt).getTime() - new Date().getTime()) / 86400000)),
+  };
+
+  let timeDifference = `${calcTime.timeDiffInDays} days ago`;
+
+  if (calcTime.timeDiffInHours < 24) {
+    timeDifference = `${calcTime.timeDiffInHours} hours ago`;
+  } else if (calcTime.timeDiffInMinutes < 60) {
+    timeDifference = `${calcTime.timeDiffInMinutes} minutes ago`;
   }
 
-  static defaultProps = {
-    title: "No Title",
-    description: "No Description",
-    urlToImage: "https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-nouvelles-nouvelles-world-global-tv-news-design-de-banni%C3%A8re.jpg",
-    publishedAt: "",
-    url: "",
-  }
-
-  render() {
-    let { title, description, urlToImage, publishedAt, url, author, source } = this.props;
-
-    let calcTime = {
-      publishedTime: new Date(publishedAt).toLocaleString(),
-      currentTime: new Date().toLocaleString(),
-      timeDiff: Math.abs(new Date(publishedAt).getTime() - new Date().getTime()),
-      timeDiffInMinutes: Math.abs(
-        Math.round((new Date(publishedAt).getTime() - new Date().getTime()) / 60000)
-      ),
-      timeDiffInHours: Math.abs(
-        Math.round((new Date(publishedAt).getTime() - new Date().getTime()) / 3600000)
-      ),
-      timeDiffInDays: Math.abs(
-        Math.round((new Date(publishedAt).getTime() - new Date().getTime()) / 86400000)
-      ),
-    };
-
-    let timeDifference = `${calcTime.timeDiffInDays} days ago`;
-
-    if (calcTime.timeDiffInHours < 24) {
-      timeDifference = `${calcTime.timeDiffInHours} hours ago`;
-    } else if (calcTime.timeDiffInMinutes < 60) {
-      timeDifference = `${calcTime.timeDiffInMinutes} minutes ago`;
-    }
-
-    return (
-      <Card style={{ margin: "auto" }}>
-        <Card.Img
-          variant='top'
-          style={{ height: "15rem", width: "100%" }}
-          src={
-            !urlToImage
-              ? "https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-nouvelles-nouvelles-world-global-tv-news-design-de-banni%C3%A8re.jpg"
-              : urlToImage
-          }
-        />
-        <Card.Body>
-          <Badge bg="primary" style={{margin: "0 0 1rem 0"}}>{!source ? "No Source" : source}</Badge>
-          <Card.Title>{!title ? "No Title" : title}</Card.Title>
-          <Card.Text>{!description ? "No Description" : description}</Card.Text>
-          <Card.Link href={url} target='_blank'>
-            Read More..
-          </Card.Link>
-        </Card.Body>
-        <Card.Footer className="d-md-flex justify-content-between">
-          <small className='text-muted'>Author : {!author ? "Unknown" : author}</small>
-          <small className='text-muted'>Last updated {timeDifference}</small>
-        </Card.Footer>
-      </Card>
-    );
-  }
-}
+  return (
+    <Card style={{ margin: "auto" }}>
+      <Card.Img
+        variant='top'
+        style={{ height: "15rem", width: "100%" }}
+        src={
+          !urlToImage
+            ? "https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-nouvelles-nouvelles-world-global-tv-news-design-de-banni%C3%A8re.jpg"
+            : urlToImage
+        }
+      />
+      <Card.Body>
+        <Badge bg='primary' style={{ margin: "0 0 1rem 0" }}>
+          {!source ? "No Source" : source}
+        </Badge>
+        <Card.Title>{!title ? "No Title" : title}</Card.Title>
+        <Card.Text>{!description ? "No Description" : description}</Card.Text>
+        <Card.Link href={url} target='_blank'>
+          Read More..
+        </Card.Link>
+      </Card.Body>
+      <Card.Footer className='d-md-flex justify-content-between'>
+        <small className='text-muted'>Author : {!author ? "Unknown" : author}</small>
+        <small className='text-muted'>Last updated {timeDifference}</small>
+      </Card.Footer>
+    </Card>
+  );
+};
 
 export default NewsItem;
+
+NewsItem.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  urlToImage: PropTypes.string,
+  publishedAt: PropTypes.string,
+  url: PropTypes.string,
+};
+
+NewsItem.defaultProps = {
+  title: "No Title",
+  description: "No Description",
+  urlToImage:
+    "https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-nouvelles-nouvelles-world-global-tv-news-design-de-banni%C3%A8re.jpg",
+  publishedAt: "",
+  url: "",
+};
