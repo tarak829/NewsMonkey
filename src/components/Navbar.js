7e050ee56b51777632b78db8639750c4d0c0cb36
tarak829 <abhishektarak45@gmail.com>
2022-03-02 18:58:43 +0530

    changed to functional based component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,43 @@
-import React, { Component } from "react";
-import { Navbar, Nav, Container} from "react-bootstrap";
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export class Component_Navbar extends Component {
-  render() {
-    return (
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Link to='/' className='navbar-brand'>
-            NewsMonkey
-          </Link>
-          <Navbar.Toggle aria-controls='navbarScroll' />
-          <Navbar.Collapse id='navbarScroll'>
-            <Nav className='me-auto my-2 my-lg-0' style={{ maxHeight: "100px" }} navbarScroll>
-              <Link to='/' className='nav-link'>
-                Home
-              </Link>
-              <Link className='nav-link' to='/business'>
-                Business
-              </Link>
-              <Link className='nav-link' to='/entertainment'>
-                Entertainment
-              </Link>
-              <Link className='nav-link' to='/health'>
-                Health
-              </Link>
-              <Link className='nav-link' to='/science'>
-                Science
-              </Link>
-              <Link className='nav-link' to='/sports'>
-                Sports
-              </Link>
-              <Link className='nav-link' to='/technology'>
-                Technology
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+const Component_Navbar = () => {
+  return (
+    <Navbar bg='dark' sticky="top" variant='dark' expand='lg'>
+      <Container fluid>
+        <Link to='/' className='navbar-brand'>
+          NewsMonkey
+        </Link>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav className='me-auto my-2 my-lg-0' style={{ maxHeight: "100px" }} navbarScroll>
+            <Link to='/' className='nav-link'>
+              Home
+            </Link>
+            <Link className='nav-link' to='/business'>
+              Business
+            </Link>
+            <Link className='nav-link' to='/entertainment'>
+              Entertainment
+            </Link>
+            <Link className='nav-link' to='/health'>
+              Health
+            </Link>
+            <Link className='nav-link' to='/science'>
+              Science
+            </Link>
+            <Link className='nav-link' to='/sports'>
+              Sports
+            </Link>
+            <Link className='nav-link' to='/technology'>
+              Technology
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Component_Navbar;
--- src/components/News.js
+++ src/components/News.js
@@ -1,149 +1,115 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Container, Row, Spinner } from "react-bootstrap";
 import NewsItem from "./NewsItem";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static propTypes = {
-    category: PropTypes.string.isRequired,
-    country: PropTypes.string.isRequired,
-    pageSize: PropTypes.number.isRequired,
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [totalResults, settotalResults] = useState(0);
 
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      totalResults: 0,
-      page: 1,
-    };
-  }
-
-  async updateNews() {
-    this.setState({ loading: true });
-    const { pageSize } = this.props;
-    const { page } = this.state;
+  const updateNews = async () => {
+    setLoading(true);
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=${pageSize}&page=${page}`
+      `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=${props.pageSize}&page=${page}`
     );
     const data = await response.json();
     if (data.status === "ok") {
-      this.setState({
-        articles: data.articles,
-        totalResults: data.totalResults,
-        loading: false,
-      });
+      setArticles(articles.concat(data.articles));
+      setPage(page + 1);
+      setLoading(false);
+      settotalResults(data.totalResults);
     } else {
-      this.setState({
-        articles: [],
-        totalResults: 0,
-        loading: false,
-      });
+      setLoading(false);
+      console.log("Error: ", data.message);
       document.title = `Error - ${data.code}`;
-      document.getElementById("header").innerHTML = `${data.message}`;
+      document.getElementById("header").innerHTML = "Error - Check logs for details";
     }
-  }
+  };
 
-  capatalize = (str) => {
+  const capatalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    this.setState({ loading: true });
-    const { pageSize } = this.props;
-    const { page } = this.state;
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    setLoading(true);
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=${pageSize}&page=${page}`
+      `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=${props.pageSize}&page=${page + 1}`
     );
     const data = await response.json();
-    this.setState({
-      articles: this.state.articles.concat(data.articles),
-      totalResults: data.totalResults,
-      loading: false,
-    });
+    setArticles(articles.concat(data.articles));
+    setLoading(false);
+    settotalResults(data.totalResults);
   };
 
-  componentDidMount() {
-    this.updateNews();
-  }
+  useEffect(() => {
+    document.title = `${capatalize(props.category)} - ${capatalize(props.country)} - NewsMonkey`;
+    updateNews();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    if (
-      prevProps.category !== this.props.category ||
-      prevProps.country !== this.props.country ||
-      prevProps.pageSize !== this.props.pageSize
-    ) {
-      this.updateNews();
-    }
-    document.title = `${this.capatalize(this.props.category)} - NewsMonkey `;
-  }
-
-  componentWillUnmount() {
-    this.setState({ articles: [] });
-  }
-
-  componentDidCatch(error, info) {
-    console.log(error, info);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 id='header' className='text-center my-4'>
-          NewsMonkey - Top {this.capatalize(this.props.category)} Headlines
-        </h1>
-        <Spinner
-          animation='border'
-          variant='primary'
-          style={{
-            display: this.state.loading ? "block" : "none",
-            margin: "5rem auto",
-            width: "3rem",
-            height: "3rem",
-          }}
-        />
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length < this.state.totalResults}
-          loader={
-            <Spinner
-              animation='border'
-              variant='primary'
-              style={{
-                display: this.state.loading ? "block" : "none",
-                margin: "5rem auto",
-                width: "3rem",
-                height: "3rem",
-              }}
-            />
-          }
-        >
-          <Container style={{ margin: "2rem auto" }}>
-            <Row xs={1} md={3} className='g-4'>
-              {this.state.articles.map((article) => (
-                <Col key={article.url}>
-                  <NewsItem
-                    title={article.title}
-                    description={article.description}
-                    url={article.url}
-                    urlToImage={article.urlToImage}
-                    publishedAt={article.publishedAt}
-                    author={article.author}
-                    source={article.source.name}
-                    emptyImage='https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-nouvelles-nouvelles-world-global-tv-news-design-de-banni%C3%A8re.jpg'
-                  />
-                </Col>
-              ))}
-            </Row>
-          </Container>
-        </InfiniteScroll>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1 id='header' className='text-center my-4'>
+        NewsMonkey - Top {capatalize(props.category)} Headlines
+      </h1>
+      <Spinner
+        animation='border'
+        variant='primary'
+        style={{
+          display: loading ? "block" : "none",
+          margin: "5rem auto",
+          width: "3rem",
+          height: "3rem",
+        }}
+      />
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length < totalResults}
+        loader={
+          <Spinner
+            animation='border'
+            variant='primary'
+            style={{
+              display: loading ? "block" : "none",
+              margin: "5rem auto",
+              width: "3rem",
+              height: "3rem",
+            }}
+          />
+        }
+      >
+        <Container style={{ margin: "2rem auto" }}>
+          <Row xs={1} md={3} className='g-4'>
+            {articles.map((article) => (
+              <Col key={article.url}>
+                <NewsItem
+                  title={article.title}
+                  description={article.description}
+                  url={article.url}
+                  urlToImage={article.urlToImage}
+                  publishedAt={article.publishedAt}
+                  author={article.author}
+                  source={article.source.name}
+                  emptyImage='https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-nouvelles-nouvelles-world-global-tv-news-design-de-banni%C3%A8re.jpg'
+                />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </InfiniteScroll>
+    </div>
+  );
+};
 
 export default News;
+
+News.propTypes = {
+  category: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  pageSize: PropTypes.number.isRequired,
+};
